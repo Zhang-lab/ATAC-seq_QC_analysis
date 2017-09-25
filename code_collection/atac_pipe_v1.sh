1956d9896750115d66b1b09cbcce60349a5a39ee
Shaopeng Liu <779611291@qq.com>
2017-09-25 18:49:10 -0500

    Create atac_pipe_v1.sh

---
+++ code_collection/atac_pipe_v1.sh
@@ -0,0 +1,9 @@
+# pipe usage:
+# user@domain: path_to_pipe/pipe.sh -g -r <PE/SE> -o read_file1 -p read_file2 (if PE file)
+# Optional parameter: -t -m -h for help
+# -t for threads number, default 24
+# -m for marker, default 'unmarked'
+# -h for help
+# input file: sra file, fastq file, and fastq.gz file
+
+

46da07571933911df3a9d61c2d8b36ab6a1c9a61
Shaopeng Liu <779611291@qq.com>
2017-09-25 23:35:10 -0500

    Update atac_pipe_v1.sh

--- code_collection/atac_pipe_v1.sh
+++ code_collection/atac_pipe_v1.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # pipe usage:
 # user@domain: path_to_pipe/pipe.sh -g -r <PE/SE> -o read_file1 -p read_file2 (if PE file)
 # Optional parameter: -t -m -h for help
@@ -6,4 +7,82 @@
 # -h for help
 # input file: sra file, fastq file, and fastq.gz file
 
+# pipe start
+###################################################################################################
+# Preparation:
+date
+
+# get the absolute path
+# readlink or realpath can do the trick, but they are not available in some version, e.g. the -f option
+pipe_loc=`dirname $0`
+cd $pipe_loc
+pipe_path=`pwd`
+
+# read parameters
+while getopts m:t:g:o:p:r:  opts
+do case "$opts" in
+m) marker="$OPTARG";;	# default 'unmarked'
+t) threads="$OPTARG";;	# default 24
+g) species="$OPTARG";;	# hg19, hg38, mm9, mm10, danRer10
+o) R1="$OPTARG";;    # PE read 1, or the SE file, or the sra file
+p) R2="$OPTARG";;    # PE read 2. 
+r) types="$OPTARG";;	# PE or SE;
+h) echo "usage:  path-to-pipe/pipe.sh  -g <hg38/hg19/mm10/mm9/danRer10/personal>  -r <PE/SE> -o read_file1  -p read_file2 (if necessary)"
+exit;;
+[?]) "Usage: ./pipe.sh  -g hg38/hg19/mm10/mm9/danRer10 -o read_file1  -r PE/SE";;
+esac
+done
+
+if [ -z "$threads" ]
+then
+threads=24
+fi
+
+if [ -z "$marker" ]
+then
+echo "you didn't specify the marker, are you sure to keep the default unmarked"
+marker='unmarked'
+fi
+
+source $pipe_path'/qc_source.sh' $species
+
+if [[ $R1 == *.sra ]]
+	then name=`echo ${R1%.sra}`
+	echo "this is sra file, $fastq_dump_tool would be used......"
+	$fastq_dump_tool $R1 --split-3
+	raw1=$name'_1.fastq'
+	raw2=$name'_2.fastq'
+elif [[ $R1 == *.fastq* ]] && [[ $types == PE  ]]
+	then
+	name=`echo ${R1%.fastq*}`
+	raw1=$R1
+	raw2=$R2
+elif [[ $R1 == *.fastq* ]] && [[ $types == SE ]]
+	then
+	name=`echo ${R1%.fastq*}`
+	raw1=$R1
+else
+	echo "please use fastq(or fastq.gz) file or sra file......"
+	exit
+fi
+
+mkdir 'Processed_'$name
+mv $raw1  ./'Processed_'$name/
+mv $raw2  ./'Processed_'$name/  2> /dev/null
+cd ./'Processed_'$name/
+mkdir 'data_collection_'$name
+touch pipe_processing.log
+
+# refine chrom_size file (remove random and Unknown record)
+awk  '{ if ((length($1) < 6) && (length($1) > 1))  print $0}' OFS='\t' $chrom_size  > refined_chrom_size.txt
+chrom_size="./refined_chrom_size.txt"
+
+# start record
+date >> pipe_processing.log
+echo "Target file is $R1 $R2" >> pipe_processing.log
+echo "Specified species is $species" >> pipe_processing.log
+echo "types of reads is $types" >> pipe_processing.log
+echo " " >> pipe_processing.log
+
+###################################################################################################
 
